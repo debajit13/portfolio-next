6052b66377d2c4f21cc4fd1dafeec0a4159a4064
debajit13 <mallickdebajit3@gmail.com>
2023-12-08 15:31:44 +0530

    Develop Blogs page UI #19

---
+++ src/components/blogs/BlogCard.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const BlogCard: React.FC<{
+  blogTitle: string;
+  blogDescription: string;
+  blogLink: string;
+  blogImage: string;
+}> = ({ blogTitle, blogDescription, blogLink, blogImage }) => {
+  return (
+    <article className='p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+      <div className='flex justify-between items-center mb-5 text-gray-500'>
+        <Image
+          className='w-full h-30 rounded-2xl'
+          src={blogImage}
+          loading='lazy'
+          height={150}
+          width={200}
+          alt={blogTitle}
+        />
+      </div>
+      <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+        {blogTitle}
+      </h2>
+      <p className='mb-5 font-light text-gray-500 dark:text-gray-400'>
+        {blogDescription}
+      </p>
+      <div className='flex justify-end items-center'>
+        <Link
+          rel='noopener noreferrer'
+          target='_blank'
+          href={blogLink}
+          className='inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline'
+        >
+          Read more
+          <svg
+            className='ml-2 w-4 h-4'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fill-rule='evenodd'
+              d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+              clip-rule='evenodd'
+            ></path>
+          </svg>
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default BlogCard;
